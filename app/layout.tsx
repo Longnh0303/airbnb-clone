b6e0ab4eb91f099a597b4e357c7b0ebc2b860944
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-09-26 17:54:15 +0700

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+import type { Metadata } from 'next'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ['latin']
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}</body>
+    </html>
+  )
+}
